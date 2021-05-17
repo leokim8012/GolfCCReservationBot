5c51b0b428cedc58de16530314bda4dd71518344
William Kim <william8012@naver.com>
2021-05-17 22:39:32 +0900

    fixed

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ Password = '8012'
 
 ReservationYear = '2021'
 ReservationMonth = '05'
-ReservationDay = '18'
+ReservationDay = '25'
 
 ReservationDate = ReservationYear + ReservationMonth + ReservationDay
 
 
 
-ReservationSeq = '024'
+ReservationSeq = '025'
 
 
 
@@ -35,30 +35,27 @@ inputPW = 'memberPw'
 LoginURL = 'https://www.daegucc.co.kr/Member/Login'
 ReservationURL = 'https://www.daegucc.co.kr/Booking/ReservationCalendar?day=' + ReservationDate
 # 
-Form = 'ReservationForm(\'160\', \'' + ReservationDay + '\', \'' + ReservationSeq + '\');'
+Form = 'ReservationForm(\'160\', \'' + ReservationDate + '\', \'' + ReservationSeq + '\');'
 ReservationOK = 'Reservation(\'ok\');'
-# Form = 'ReservationForm('160','20210512','001');'
 
-# driver = webdriver.Chrome(executable_path='chromedriver')
+driver = webdriver.Chrome(executable_path='chromedriver')
 
-# #Goto Login
-# driver.get(url=LoginURL)
-# #Login
-# idBox = driver.find_element_by_id(inputID)
-# idBox.send_keys(ID)
-# pwBox = driver.find_element_by_id(inputPW)
-# pwBox.send_keys(Password)
-# driver.execute_script('login()')
+#Goto Login
+driver.get(url=LoginURL)
+#Login
+idBox = driver.find_element_by_id(inputID)
+idBox.send_keys(ID)
+pwBox = driver.find_element_by_id(inputPW)
+pwBox.send_keys(Password)
+driver.execute_script('login()')
 
-# #Login alert
-# result = driver.switch_to_alert()
-# result.accept()
-
+#Login alert
+result = driver.switch_to_alert()
+result.accept()
 
 
 
-# #Goto Reservation
-# driver.get(url=ReservationURL)
+
 
 
 targetYear = int(ReservationYear)
@@ -77,18 +74,44 @@ now = datetime.now()
 prev_time = now
 
 
+print('현재 시간', now)
+print('예약 시간', targetDate)
+
+driver.get(url=ReservationURL)
 while(1):
   now = datetime.now()
   countDown = targetDate - now
 
+  if(now.second - prev_time.second >= 1):
+    print('예약까지 남은 시간:', countDown)
 
-  print('예약까지 남은 시간:', countDown)
+  if(now.year == targetDate.year and now.month == targetDate.month and now.day == targetDate.day and now.hour == targetDate.hour and now.minute == targetDate.minute and now.second == targetDate.second):
+    startingTime = now
+    print('시작 시간', startingTime)
 
-  prev_second = now.second
 
-  if(now.date == targetDate):
+    #Goto Reservation
+    print('폼 입력 중')
     #Send Form
-    # driver.execute_script(Form)
-    # driver.execute_script(ReservationOK)
+    print(Form)
+    driver.execute_script(Form)
+    print('폼 작성 완료')
+
+    print('예약 확인 중')
+
+    print(ReservationOK)
+    driver.execute_script(ReservationOK)
+    print('예약 완료')
+    
+    result = driver.switch_to_alert()
+    result.accept()
+
+    endTime = now
+    print('종료 시간', endTime)
+    print('총 소요 시간: ', endTime - startingTime)
     break
+
+
+
+  prev_time = now
 
